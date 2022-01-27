58881eca31d56020b1643ba8b12a054f1f0d82d0
Papus <foal0912@gmail.com>
2022-01-27 13:34:16 +0900

    웹캠 출력 구현. python version 3.8.10

---
+++ audiotest/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import include
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('CVtest', views.detectme, name='detectme'),
+    path('interview', views.interview, name='interview'),
+    path('interview/setting', views.interview_setting, name='setting'),
+    path('interview/main', views.interview_main, name='main'),
+    path('mypage', views.mypage, name='mypage'),
+    path('mypage/privacy', views.mypage_privacy, name='privacy'),
+    path('mypage/history', views.mypage_history, name='history'),
+]
---
+++ audiotest/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+import cv2
+import threading
+from django.utils import timezone
+
+# Create your views here.
+"""
+모의면접 화면 출력
+"""
+def index(request):
+    return render(request, 'index.html')
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def detectme(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        print("에러입니다...")
+        pass
+
+def interview(request):
+    return render(request, 'interview.html')
+
+def interview_setting(request):
+    return render(request, 'interview_question_setting.html')
+
+def interview_main(request):
+    return render(request, 'interview_main.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
+
+def mypage_privacy(request):
+    return render(request, 'mypage_privacy.html')
+
+def mypage_history(request):
+    return render(request, 'mypage_history.html')
