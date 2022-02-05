6827c5fd0c4dcaac9d3f1751cdb20c1ef09b4dcd
Papus <foal0912@gmail.com>
2022-02-05 18:46:20 +0900

    동영상 저장 모델 및 경로 추가

--- audiotest/admin.py
+++ audiotest/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import QuestionChar
+from .models import QuestionChar, InterviewStorage
 
 admin.site.register(QuestionChar)
+admin.site.register(InterviewStorage)
 # Register your models here.
--- config/settings.py
+++ config/settings.py
@@ -127,4 +127,7 @@ STATICFILES_DIR = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [
+    BASE_DIR / 'media',
+]
