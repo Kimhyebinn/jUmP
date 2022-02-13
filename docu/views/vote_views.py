b34f9673dd59b1bf78a623e680e032adae7f1693
khb <khb@mysite.com>
2022-02-14 00:44:15 +0900

    docu 수정

--- docu/views/vote_views.py
+++ docu/views/vote_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 
-from ..models import Question, Answer
+from ..models import Question
 
 
 @login_required(login_url='common:login')
@@ -16,15 +16,3 @@ def vote_question(request, question_id):
     else:
         question.voter.add(request.user)
     return redirect('docu:detail', question_id=question.id)
-
-@login_required(login_url='common:login')
-def vote_answer(request, answer_id):
-    """
-    docu 답글 추천 등록
-    """
-    answer = get_object_or_404(Answer, pk=answer_id)
-    if request.user == answer.author:
-        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
-    else:
-        answer.voter.add(request.user)
-    return redirect('docu:detail', question_id=answer.question.id)
